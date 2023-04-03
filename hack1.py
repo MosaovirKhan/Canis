f881a3cbdcdc3f9206664daa72be8f0104fc9099
MosaovirKhan <naozif345@gmail.com>
2023-04-03 01:41:43 -0600

    elbow map implemented, need to make it faster

--- hack1.py
+++ hack1.py
@@ -3,6 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 def download_nltk_module(module_name):
     try:
@@ -11,11 +14,11 @@ def download_nltk_module(module_name):
         nltk.download(module_name)
 
 # Load the real data CSV file
-real_data = pd.read_csv("DataSet_Misinfo_TRUE.csv", usecols=[1], names=["text"], skiprows=1, nrows = 30)
+real_data = pd.read_csv("DataSet_Misinfo_TRUE.csv", usecols=[1], names=["text"], skiprows=1, nrows = 300)
 real_data["label"] = "real"
 
 # Load the fake data CSV file
-fake_data = pd.read_csv("DataSet_Misinfo_FAKE.csv", usecols=[1], names=["text"], skiprows=1, nrows= 30)
+fake_data = pd.read_csv("DataSet_Misinfo_FAKE.csv", usecols=[1], names=["text"], skiprows=1, nrows= 300)
 fake_data["label"] = "fake"
 
 # Combine the real and fake data into one DataFrame
@@ -23,10 +26,10 @@ combined_data = pd.concat([real_data, fake_data], ignore_index=True)
 print("Data combined into one DataFrame")
 
 # Download required nltk resources if not already present
-download_nltk_module('punkt')
-download_nltk_module('stopwords')
-download_nltk_module('wordnet')
-print("nltk modules downloaded")
+# download_nltk_module('punkt')
+# download_nltk_module('stopwords')
+# download_nltk_module('wordnet')
+# print("nltk modules downloaded")
 
 # Define preprocessing function
 def preprocess_text(text):
@@ -60,5 +63,21 @@ def preprocess_text(text):
 # Apply the preprocessing function to the text column
 combined_data['text'] = combined_data['text'].apply(preprocess_text)
 
-print(combined_data.head(5))
-#hello
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(combined_data['text'])
+
+inertia_values = []
+num_clusters = range(1, 11)
+
+for k in num_clusters:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X)
+    inertia_values.append(kmeans.inertia_)
+
+plt.plot(num_clusters, inertia_values, 'bx-')
+plt.xlabel('Number of clusters (K)')
+plt.ylabel('Inertia')
+plt.title('Elbow Method for Optimal K')
+plt.show()
+
+
